3728a26ec80da0f76a4240a4cdc47b81bc4134c3
gingagenie <167283582+gingagenie@users.noreply.github.com>
2025-11-07 12:47:49 +1100

    Import jobPhotos route in index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import jobPhotos from "./routes/job-photos";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";

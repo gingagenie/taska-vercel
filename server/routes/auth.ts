84dcfad778a424f31defacb6319ff5449bfb42c7
gingagenie <167283582+gingagenie@users.noreply.github.com>
2026-01-07 20:23:56 +1100

    Update auth.ts

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { db } from "../db/client";
 import { sql } from "drizzle-orm";
-import bcrypt from "bcryptjs";
 import multer from "multer";
 import path from "path";
 import fs from "fs";
